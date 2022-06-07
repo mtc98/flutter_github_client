9e55755a801b2b7dd5081c8e659dfa19c66bcab8
Thomas Mei <mtc98@me.com>
2022-06-07 16:10:59 +0800

    First commit from mtc

---
+++ android/app/src/main/kotlin/com/mtc98/github_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mtc98.github_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// 2個 Id Secret 值
+const githubClientId = 'c11c28aecfed24ce3f12';
+const githubClientSecret = '001642372bbde38674762d0e6bcce0b77942e9c6';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
